8eac62c5e4f2b1ac20bc6fd7e246c7ebc215c56a
chiradeep <chiradeep13@gmail.com>
2022-04-03 00:52:27 +0530

    Tried to rethink

--- src/XOR.js
+++ src/XOR.js
@@ -7,51 +7,59 @@ const { matrix,
     add,
     divide,
     det,
-    transpose,
-    subtract } = require('mathjs');
+    subtract, 
+    inv,
+    map,
+    transpose} = require('mathjs');
 
 let x1 = [];
 let x2= [];
 
 let y= [];
 
+let weights = matrix([[1],[1]]);
+let bias = matrix([[1]]);
 
 //Simple perceptron
 const sigmoid = (x) => {
     return divide(1, add(1,exp(multiply(-1,x))));
 };
 const sigmoidDerivative = (x) => {
-    return multiply(sigmoid(x),subtract(1,sigmoid(x)));
+    let sig = sigmoid(x);
+    return multiply(sig,subtract(1,sig));
 };
 
-let weights = matrix([[0, 0]]);
-let bias = matrix([[1]]);
-
-const forwordPropagation=(X,W,B) => {
-    let Z1 = add(multiply(W,X),B);
+const forwordPropagation=(X,weights,bias) => {
+    let Z1 = add(multiply(X,weights),bias);
     let Y = sigmoid(Z1);
     return {Y,Z1};
 };
 
-const backPropagation=(X,Y,Y_lable,Z1) => {
-    let delta = subtract(Y,Y_lable);
+const backPropagation=(Y,Y_lable,X)=>{
+    let dY = subtract(Y,Y_lable);
+    let dZ1 = sigmoidDerivative(Y);
+    let denominator_W = multiply(transpose(X),dZ1);
+    denominator_W = map(denominator_W,(x)=>{
+        return 1/x;
+    });
+    let dW = multiply(denominator_W,dY);
+    let dB = divide(dY,dZ1);
 
-    let dW = multiply(sigmoidDerivative(Z1),multiply(multiply(2,delta),transpose(X)));
-    let dB = multiply(sigmoidDerivative(Z1),multiply(delta,matrix([[1]])));
-
-    return {dW,dB};
+    return { dW, dB };
 };
 
-const train = (X,Y_lable,W,B,learningRate,epoch) => {
-    for(let i=0;i<epoch;i++){
-        let {Y,Z1} = forwordPropagation(X,W,B);
-        let {dW,dB} = backPropagation(X,Y,Y_lable,Z1,W,B);
+const train = (X,Y_lable,learning_rate,epoch)=>{
 
-        W = subtract(W,multiply(learningRate,dW));
-        B = subtract(B,multiply(learningRate,dB));
+    for(let i = 0 ; i<epoch;i++){
+        let {Y,Z1} = forwordPropagation(X,weights,bias);
+        let {dW,dB} = backPropagation(Y,Y_lable,X);
+        console.log(dW,dB);
+        weights = subtract(weights,multiply(learning_rate,dW));
+        bias = subtract(bias,multiply(learning_rate,dB));
     }
-    return {W,B};
-};
+
+    return {weights,bias};
+}
 
 
 createReadStream('/home/banik/Desktop/Code/SimpleNN/DATA/XOR/Xor_Dataset.csv')
@@ -71,30 +79,26 @@ createReadStream('/home/banik/Desktop/Code/SimpleNN/DATA/XOR/Xor_Dataset.csv')
         let testing_x2 = x2.splice(0, x2.length);
         let testing_y = y.splice(0, y.length);
 
-        for (let i =0;i<training_x1.length;i++){
-            let X = matrix([[training_x1[i]],[training_x2[i]]]);
-            let Y_lable = matrix([[training_y[i]]]);
-            let {W,B} = train(X,Y_lable,weights,bias,0.1,10);
-            if(i%100===0){
-                console.log(`Training -- ${i+1}`);
-                let result=0;
-                for(let k=0;k<testing_x1.length;k++){
-                    let X_Test = matrix([[testing_x1[k]],[testing_x2[k]]]);
-                    let Y_lable_Test = matrix([[testing_y[k]]]);
-                    let { Y } = forwordPropagation(X_Test,W,B);
-                    if(det(Y)>0.5){
-                        if(det(Y_lable_Test)==1){
-                            result++;
-                        }
-                    }else if(det(Y)<0.5){
-                        if(det(Y_lable_Test)==0){
-                            result++;
-                        }
+        for (let i =0;i<1;i++){
+            let X = matrix([[parseInt(training_x1[i]),parseInt(training_x2[i])]]);
+            let Y_lable = matrix([[parseInt(training_y[i])]]);
+            let {weights,bias} = train(X,Y_lable,0.1,100);
+            if(i%100 == 0){
+                console.log(weights,bias);
+                let result =0;
+                for(let j=0;j<0;j++){
+                    let X = matrix([[parseInt(testing_x1[j]),parseInt(testing_x2[j])]]);
+                    let Y_lable = matrix([[parseInt(testing_y[j])]]);
+                    let {Y,Z1} = forwordPropagation(X,weights,bias);
+                    if(Y.get([0,0])>0.5 && Y_lable.get([0,0])== 1){
+                        result++;
+                    }
+                    if(Y.get([0,0])<0.5 && Y_lable.get([0,0]) == 0){
+                        result--;
                     }
                 }
-                console.log(`Accuracy -- ${result/testing_x1.length}`);
+                console.log("Accuracy --- " , (result/testing_x1.length)*100 , "%");
             }
         }
-
     });
 
